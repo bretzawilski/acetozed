cfca804a754d5531e7fe7f16525ae7d60128a7c4
Bret Zawilski <bret.zawilski@gmail.com>
2022-08-24 11:10:55 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function (eleventyConfig) {
+  
+  // Copy `src/style.css` to `_site/style.css`
+  eleventyConfig.addPassthroughCopy("src/style.css");
+
+  // Set custom directories for input, output, includes, and data
+  return {
+        // When a passthrough file is modified, rebuild the pages:
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "data",
+      output: "_site"
+    }
+  };
+};
